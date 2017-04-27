ff567b320349fbe0624a998dd4f27727159ce5ad
Josh Seipel <joshseipel@gmail.com>
2017-04-27 16:23:29 -0400

    Added a add button to city show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,18 @@
 class PostsController < ApplicationController
 
+def show
+	@post = Post.find(params[:id])
+	@post.title = @post.title.upcase
+	@user = User.find(@post.user_id)
+
+
+end
+
+def new
+
+
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   }
 
   resources :cities
+  resources :posts
 
 
 
