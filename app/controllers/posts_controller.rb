5d93983270c242b8dacd73093286b517f3425879
Josh Seipel <joshseipel@gmail.com>
2017-04-27 18:07:43 -0400

    Branch out to Add to Post Create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,31 @@
 class PostsController < ApplicationController
+before_action :authenticate_user!
 
-def show
-	@post = Post.find(params[:id])
-	@post.title = @post.title.upcase
-	@user = User.find(@post.user_id)
+	def show
+		@post = Post.find(params[:id])
+		@post.title = @post.title.upcase
+		@user = User.find(@post.user_id)
+	end
 
+	def new
+		# @city = City.find(params[:city_id])
+		@post = Post.new
+	end
 
+	def create
+		post = Post.create(post_params)
+		city = post.city
+		# binding.pry
+		redirect_to city_post_path(city, post)
+	end
+
+	private
+	def post_params
+		params.require(:post)
+		.permit(:title, :content)
+		.merge(user_id: current_user.id, city_id: params[:city_id])
+	end
 end
 
-def new
 
 
-end
-
-
-
-end
