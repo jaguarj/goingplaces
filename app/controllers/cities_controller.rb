219f9f5486050d9d97603c688d6fdd12858f6a30
Josh Seipel <joshseipel@gmail.com>
2017-04-27 15:40:17 -0400

    created at sort by for post

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -6,7 +6,7 @@ class CitiesController < ApplicationController
 
   def show
   	@city = City.find(params[:id])
-  	@posts = @city.posts
+  	@posts = @city.posts.sort_by { |post| post.created_at}
   end
 
 end
