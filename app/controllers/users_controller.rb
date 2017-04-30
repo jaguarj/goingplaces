6bcc9891c6af630dfce2e98cb6133a54bdc0ea4c
hernquist <hernquistdavid@gmail.com>
2017-04-30 10:24:42 -0400

    sprint 3 bonuses 1 and 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
+
   def show
-  @user = current_user
-  @posts = @user.posts
+  	@user = current_user
+  	@posts = @user.posts
+
+  	#I need to get cities so that I can make a for loop through them
+  	@cities = City.all
+  	# this makes an array that stores the occurance of cities by city_id 
+  	@city_user_info = @user.cities.select(:id).map { |c| c.id }
+  	
   end
+
 end
